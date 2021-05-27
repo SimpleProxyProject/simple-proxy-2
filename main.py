bc6db5518ae340a3553b3b970ef18c76805a89ec
Rehmat Alam <contact@rehmat.works>
2021-05-27 21:13:53 +0500

    Delete host field

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def root(url: str, request: Request):
         params = dict(request.query_params)
         del params['url']
         headers = dict(request.headers)
+        del headers['host']
+        if params.get('host'):
+            headers['host'] = params.get('host')
         cookies = dict(request.cookies)
         if len(params) > 0:
             url += f'?{urlencode(params)}'
