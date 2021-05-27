27fac0650a69163c626cb0b5d24bdeafd18d5a37
Rehmat Alam <contact@rehmat.works>
2021-05-27 21:16:26 +0500

    Accept all

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def root(url: str, request: Request):
         del headers['host']
         if params.get('host'):
             headers['host'] = params.get('host')
+        del headers['accept-encoding']
+        headers['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
         cookies = dict(request.cookies)
         if len(params) > 0:
             url += f'?{urlencode(params)}'
