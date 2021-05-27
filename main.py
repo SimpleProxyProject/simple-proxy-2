6801bb21e03c2d7877686c68cae6e3a532d448d5
Rehmat Alam <contact@rehmat.works>
2021-05-27 22:47:00 +0500

    Custom user agents

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from fastapi.responses import PlainTextResponse
 from starlette.status import HTTP_403_FORBIDDEN
 import requests
 from urllib.parse import urlencode
+import random
 import os
 
 
 app = FastAPI()
-SECRET_KEY = os.environ.get('APIKEY')
-apikey = APIKeyHeader(name='APIKEY', auto_error=False)
+SECRET_KEY = os.environ.get('SIMPLEPROXYSECRET')
+apikey = APIKeyHeader(name='SIMPLEPROXYKEY', auto_error=False)
 
 
 def get_api_key(apikey: str = Security(apikey)):
@@ -21,20 +22,56 @@ def get_api_key(apikey: str = Security(apikey)):
 
 
 @app.get('/', response_class=PlainTextResponse)
-def root(url: str, request: Request, api_key: APIKey = Depends(get_api_key)):
+def root(proxyurl: str, request: Request, api_key: APIKey = Depends(get_api_key)):
     try:
+        # Get query params
         params = dict(request.query_params)
-        del params['url']
+
+        # Delete proxyurl param
+        del params['proxyurl']
+
+        # Get headers
         headers = dict(request.headers)
-        del headers['host']
-        del headers['APIKEY']
+
+        # Remove unwanted headers
+        ignore_list = ['host', 'SIMPLEPROXYKEY', 'accept-encoding']
+        for header in ignore_list:
+            del headers[header]
+
+        # Set custom host header if provided
         if params.get('host'):
             headers['host'] = params.get('host')
-        del headers['accept-encoding']
+
+        # Set accept header
         headers['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+
+        # Set custom user agent if requested. The list has been obtained from https://whatismybrowser.com
+        user_agent = None
+        desktop_useragents = [
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
+            ]
+        mobile_useragents = [
+            'Mozilla/5.0 (Linux; Android 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36']
+        if params.get('simpleproxy_device') == 'desktop':
+            user_agent = random.choice(desktop_useragents)
+        if params.get('simpleproxy_device') == 'mobile':
+            user_agent = random.choice(mobile_useragents)
+        if user_agent is not None:
+            headers['user-agent'] = user_agent
+
+        # Get cookies from request
         cookies = dict(request.cookies)
+
+        # Encode params & update proxy URL
         if len(params) > 0:
-            url += f'?{urlencode(params)}'
-        return str(requests.get(url, headers=headers, cookies=cookies, timeout=5).text)
+            proxyurl += f'?{urlencode(params)}'
+
+        # Make external request and return response
+        return str(requests.get(proxyurl, headers=headers, cookies=cookies, timeout=5).text)
     except:
         return 'Request failed!'
