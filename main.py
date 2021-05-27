f957795a7313973c7f321061523d587d3cd6375f
Rehmat Alam <contact@rehmat.works>
2021-05-27 21:01:18 +0500

    Timeout set

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 import requests
+from urllib.parse import urlparse, parse_qs
 
 
 app = FastAPI()
 
 
-@app.get('/', response_class=PlainTextResponse)
-async def root(url: str, device: str = 'desktop'):
-    if device == 'mobile':
-        user_agent = 'Mozilla/5.0 (Linux; Android 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36'
-    else:
-        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
-
-    headers = {
-        'referrer': 'https://google.com',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Pragma': 'no-cache',
-        'user-agent': user_agent
-    }
-
+@app.get('/')
+def root(url: str, request: Request):
     try:
-        return str(requests.get(url, headers=headers).text)
+        params = dict(request.query_params)
+        del params['url']
+        headers = dict(request.headers)
+        cookies = dict(request.cookies)
+        return str(requests.get(url, headers=headers, cookies=cookies, params=params, timeout=5).text)
     except:
         return 'Request failed!'
