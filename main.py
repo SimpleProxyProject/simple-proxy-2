70f3c3b9f25c6421ec2c8ed84b78ada6acf2892c
Rehmat Alam <contact@rehmat.works>
2023-07-20 13:49:08 +0500

    Use requests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import random
 from urllib.parse import urlencode
-import httpx
+import requests
 from fastapi import Security, Depends, FastAPI, HTTPException, Request, Response
 from fastapi.security.api_key import APIKeyHeader, APIKey
 from fastapi.responses import PlainTextResponse
@@ -23,13 +23,14 @@ def get_api_key(apikey: str = Security(apikey)):
 
 
 @app.get('/version')
-async def get_version():
+def get_version():
     return {
         'version': '3.0.1'
     }
 
+
 @app.get('/', response_class=PlainTextResponse, status_code=200)
-async def root(url: str, request: Request, response: Response, api_key: APIKey = Depends(get_api_key)):
+def root(url: str, request: Request, response: Response, api_key: APIKey = Depends(get_api_key)):
     status_code = 500
     try:
         # Get query params
@@ -49,20 +50,27 @@ async def root(url: str, request: Request, response: Response, api_key: APIKey =
             headers['host'] = params.get('host')
 
         # Set accept header
-        headers['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        headers['accept'] = 'text/html,application/xhtml+xml,application/xml;' \
+                            'q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
 
         # Set custom user agent if requested. The list has been obtained from https://whatismybrowser.com
         user_agent = None
         desktop_useragents = [
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
+            ' (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
             'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
             ]
         mobile_useragents = [
-            'Mozilla/5.0 (Linux; Android 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
-            'Mozilla/5.0 (Linux; Android 11; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
-            'Mozilla/5.0 (Linux; Android 11; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
-            'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36']
+            'Mozilla/5.0 (Linux; Android 11) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; SM-A205U) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; SM-A102U) AppleWebKit/537.36'
+            ' (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36']
         if params.get('simpleproxy_device') == 'desktop':
             user_agent = random.choice(desktop_useragents)
         if params.get('simpleproxy_device') == 'mobile':
@@ -83,7 +91,6 @@ async def root(url: str, request: Request, response: Response, api_key: APIKey =
         if len(params) > 0:
             url += f'?{urlencode(params)}'
 
-        
         # Proxy setup
         if PROXY_PATH:
             proxies = {
@@ -94,29 +101,28 @@ async def root(url: str, request: Request, response: Response, api_key: APIKey =
             proxies = None
 
         # Make external request and return response
-        async with httpx.AsyncClient(proxies=PROXY_PATH, headers=headers, cookies=cookies, timeout=3) as client:
-            resp = await client.get(url)
-            status_code = int(resp.status_code)
-            resp.raise_for_status()
-            return resp.text
+        resp = requests.get(url, proxies=proxies, headers=headers, cookies=cookies, timeout=3)
+        status_code = int(resp.status_code)
+        resp.raise_for_status()
+        return resp.text
     except Exception as e:
         response.status_code = status_code
         return f'Request failed: {str(e)}'
 
+
 @app.get('/ping')
-async def ping():
+def ping():
     return {
         'status': True
     }
 
+
 @app.get('/ip')
-async def get_ip():
+def get_ip():
     try:
-        async with httpx.AsyncClient(proxies=PROXY_PATH) as client:
-            resp = await client.get('http://ip-api.com/json')
-            ip = resp.json().get('query')
-    except Exception as e:
-        raise e
+        resp = requests.get('http://ip-api.com/json', proxies={'http': PROXY_PATH})
+        ip = resp.json().get('query')
+    except requests.RequestException:
         ip = None
     return {
         'ip': ip
